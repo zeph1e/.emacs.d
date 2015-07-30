237f48fcd7bd23c09e5b2e7744f339153787bd8d
Yunsik Jang <yunsik.jang@lge.com>
2015-07-30 10:32:59 +0900

    Remove erc-opt

--- utils/erc-opt.el
+++
@@ -1,9 +0,0 @@
-(defun my:erc-setup ()
-  (setq erc-nick "zeph1e")
-  (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs"))
-	erc-autojoin-mode t)
-  (set-face-foreground 'erc-input-face "ivory"))
-
-(if (boundp 'with-eval-after-load)
-    (with-eval-after-load (my:erc-setup))
-  (add-hook 'erc-mode-hook 'my:erc-setup))
