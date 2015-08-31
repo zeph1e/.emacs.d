4fb09ccec78221035a160200e07aa11fa58b993b
Yunsik Jang <yunsik.jang@lge.com>
2015-08-31 17:55:49 +0900

    Add babel.el ansl.el
    babel.el : babel configuration
    ansi.el : ansi color setting

---
+++ utils/ansi.el
@@ -0,0 +1,6 @@
+;;; ansi.el -- set ansi color
+
+(setq ansi-color-names-vector
+      ["black" "tomato" "green" "gold1"
+       "DeepSkyBlue1" "Mediu8mOrchid1" "cyan" "white"])
+;; (setq ansi-color-map (ansi-color-make-color-map))
---
+++ utils/babel.el
@@ -0,0 +1,27 @@
+;;; babel.el -- org-babel configuration
+
+(defconst babel-language-alist '((awk . t)
+                                 (C . t)
+                                 (cpp . t)
+                                 (css . t)
+                                 (emacs-lisp . t)
+                                 (gnuplot . t)
+                                 (java . t)
+                                 (python . t)
+                                 (plantuml . t)
+                                 (sh . t)))
+
+(define-minor-mode babel-language-mode
+  "inject org-babel settings on org-mode-hook."
+  nil nil nil
+  (require 'ob-awk)
+  (require 'ob-C)
+  (require 'ob-css)
+  (require 'ob-emacs-lisp)
+  (require 'ob-gnuplot)
+  (require 'ob-java)
+  (require 'ob-python)
+  (require 'ob-plantuml) (setq org-plantuml-jar-path "~/.emacs.d/el-get/plantuml-mode/plantuml.jar")
+  (require 'ob-sh)
+  (org-babel-do-load-languages 'org-babel-do-load-languages babel-language-alist))
+(add-hook 'org-mode-hook 'babel-language-mode)
