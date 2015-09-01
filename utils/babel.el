4a764e511d70f8d8cfa0ba0621e6e835f2ff1bcd
Yunsik Jang <yunsik.jang@lge.com>
2015-09-01 11:47:24 +0900

    Rearrange babel settings

--- utils/babel.el
+++ utils/babel.el
@@ -1,26 +1,37 @@
 ;;; babel.el -- org-babel configuration
 
-(defconst babel-language-alist '((awk . t)
-                                 (C . t)
-                                 (cpp . t)
-                                 (css . t)
-                                 (emacs-lisp . t)
-                                 (gnuplot . t)
-                                 (java . t)
-                                 (python . t)
-                                 (plantuml . t)
-                                 (sh . t)))
+(defconst babel-language-alist '((awk . ob-awk)
+                                 (C . ob-C)
+                                 (cpp . ob-C)
+                                 (css . ob-css)
+                                 (emacs-lisp . ob-emacs-lisp)
+                                 (gnuplot . ob-gnuplot)
+                                 (java . ob-java)
+                                 (python . ob-python)
+                                 (plantuml . ob-plantuml)
+                                 (sh . ob-sh)))
 
 (eval-after-load 'org
   '(progn
-     (require 'ob-awk)
-     (require 'ob-C)
-     (require 'ob-css)
-     (require 'ob-emacs-lisp)
-     (require 'ob-gnuplot)
-     (require 'ob-java)
-     (require 'ob-python)
-     (require 'ob-plantuml) (setq org-plantuml-jar-path "~/.emacs.d/el-get/plantuml-mode/plantuml.jar")
-     (require 'ob-sh)
+     (let (load-languages require-list)
+       (dolist (lang babel-language-alist)
+         (push (cons (car lang) t) load-languages)
+         (add-to-list 'require-list (cdr lang)))
+       (dolist (req require-list)
+         (require req))
+       (org-babel-do-load-languages 'org-babel-do-load-languages load-languages))
      (org-babel-do-load-languages 'org-babel-do-load-languages babel-language-alist)
-     (setq org-confirm-babel-evaluate nil)))
+
+     ;; additional settings for each languages
+     (setq org-plantuml-jar-path "~/.emacs.d/el-get/plantuml-mode/plantuml.jar")
+
+     ;; other settings
+     (setq org-confirm-babel-evaluate nil) ; turn off annoying prompt
+
+     ;; inline images setting
+     (when (display-graphic-p)
+       (setq org-startup-with-inline-images t)
+       (add-hook 'org-babel-after-execute-hook (lambda ()
+                                                 (when org-inline-image-overlays
+                                                   (org-redisplay-inline-images)))))))
+
