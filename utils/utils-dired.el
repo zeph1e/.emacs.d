6a683f38b7f7f4ff36c648241738b242c60d09f0
Yunsik Jang <yunsik.jang@lge.com>
2015-10-14 16:18:35 +0900

    Rename utils-magit.el -> utils-dired.el

--- utils/utils-dired.el
+++ utils/utils-dired.el
@@ -1,26 +1,48 @@
-;;; utils-magit.el
+;;; utils-dired.el
 
-(when (eq system-type 'windows-nt) ; to resolve push problems in windows
-  (setenv "GIT_ASKPASS" "git-gui--askpass"))
-
-(defvar my:magit-mode-keymap
+(defvar my:dired-mode-keymap
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "@ f")  'my:magit-file-log)
+    (define-key map (kbd "V")    'my:view-file-external)
+    (define-key map (kbd "@ f")  'my:git-file-log)
+    (define-key map (kbd "@ t")  'my:git-stage-file)
     (define-key map (kbd "@ g")  'magit-git-command)
     (define-key map (kbd "@ i")  'magit-init)
     (define-key map (kbd "@ s")  'magit-status)
     (define-key map (kbd "@ l")  'magit-log)
     map)
-  "my:magit keymap for dired.")
+  "my:dired-mode keymap for dired.")
 
-(defun my:magit-file-log (file &optional use-graph)
+(defvar my:view-file-opener
+  (eval-when-compile
+    (or (executable-find "explorer.exe") (executable-find "xdg-open"))))
+
+(defun my:view-file-external (file)
+  (interactive (list (dired-file-name-at-point)))
+  (call-process-shell-command (concat my:view-file-opener " " (shell-quote-argument (expand-file-name file)))))
+
+(defun my:git-stage-file (file)
   (interactive
    (progn
      (unless (fboundp 'magit-get-current-branch)
        (require 'magit))
      (if (< 2 (length (remove '&optional (help-function-arglist 'magit-read-file-from-rev))))
          (list (magit-read-file-from-rev (magit-get-current-branch)
-                                         "File"
+                                         "Git stage file"
+                                         (magit-file-relative-name (dired-file-name-at-point)))
+               current-prefix-arg)
+       (list (magit-read-file-from-rev (magit-get-current-branch)
+                                       (magit-file-relative-name (dired-file-name-at-point)))
+             current-prefix-arg))))
+  (magit-stage-file file))
+
+(defun my:git-file-log (file &optional use-graph)
+  (interactive
+   (progn
+     (unless (fboundp 'magit-get-current-branch)
+       (require 'magit))
+     (if (= 3  (length (remove '&optional (help-function-arglist 'magit-read-file-from-rev))))
+         (list (magit-read-file-from-rev (magit-get-current-branch)
+                                         "Git log file"
                                          (magit-file-relative-name (dired-file-name-at-point)))
                current-prefix-arg)
        (list (magit-read-file-from-rev (magit-get-current-branch)
@@ -30,13 +52,13 @@
       (magit-file-log file use-graph)
     (magit-log-head '("--decorate") (list file))))
 
-(define-minor-mode my:magit-mode
+(define-minor-mode my:dired-mode
   "Add some keybindings into dired mode.
 
 Key bindings:
-\\{my:magit-mode-keymap}"
-  nil nil my:magit-mode-keymap)
-(add-hook 'dired-mode-hook 'my:magit-mode)
+\\{my:dired-mode-keymap}"
+  nil nil my:dired-mode-keymap)
+(add-hook 'dired-mode-hook 'my:dired-mode)
 
 ;; (define-minor-mode my:magit-branch-mode
 ;; "Show git branch if it's in git controlled directory."
---
+++ utils/utils-git.el
@@ -0,0 +1,4 @@
+;;; utils-git.el
+
+(when (eq system-type 'windows-nt) ; to resolve push problems in windows
+  (setenv "GIT_ASKPASS" "git-gui--askpass"))
