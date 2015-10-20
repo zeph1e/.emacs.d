b491a8edf0ad85f41e151f23d87c1b756c6d00a3
Yunsik Jang <yunsik.jang@lge.com>
2015-10-20 16:03:38 +0900

    Add utils-auto-complete.el

---
+++ utils/utils-auto-complete.el
@@ -0,0 +1,24 @@
+;;; utils-auto-complete.el
+
+;; Written by Yunsik Jang <doomsday@kldp.org>
+;; You can use/modify/redistribute this freely.
+
+;; include subdirectories like gtk+-2.0 or QtCore
+(defconst my:achead-subdir-regexp "[A-Za-z0-9-_]+\\-[0-9.]+\\|Qt[A-Za-z]+")
+(defvar my:achead-searched-subdir nil)
+(unless my:achead-searched-subdir
+  (let* ((cc (or (getenv "CC") (executable-find "gcc") (executable-find "cc")))
+         (arch (and cc (substring (shell-command-to-string (concat cc " -dumpmachine")) 0 -1)))
+         (readd (and (executable-find "uname")
+                     (string-prefix-p (substring (shell-command-to-string "uname -m") 0 -1) arch)
+                     (concat "\\|" arch)))
+         includedirs)
+    (dolist (d achead:include-directories)
+      (and (file-directory-p d)
+           (string-prefix-p "/" d)
+           (dolist (subd (directory-files d nil (concat my:achead-subdir-regexp (or readd ""))))
+             (and (file-directory-p (expand-file-name subd d))
+                  (push (expand-file-name subd d) includedirs)))))
+    (setq achead:include-directories(append achead:include-directories includedirs)
+          my:achead-searched-subdir t)))
+
