f4681f96a3c3075aa22a27211dc4f95fcb0123f7
Yunsik Jang <yunsik.jang@lge.com>
2015-11-16 17:14:34 +0900

    Fix error in parenthesis

--- utils/utils-google-translate.el
+++ utils/utils-google-translate.el
@@ -6,5 +6,5 @@
 (eval-after-load 'google-translate
   '(progn
      (setq google-translate-default-source-language "auto")
-     (setq google-translate-default-target-language "ko"))
+     (setq google-translate-default-target-language "ko")))
 (global-set-key (kbd "M-+") 'google-translate-at-point)
