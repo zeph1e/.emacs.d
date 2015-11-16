9a5ecdd49e9bb12d64cb77a6b1ffcf3a0a361782
Yunsik Jang <yunsik.jang@lge.com>
2015-11-16 17:30:26 +0900

    Fix variable setting timing

--- utils/utils-google-translate.el
+++ utils/utils-google-translate.el
@@ -3,10 +3,12 @@
 ;; Written by Yunsik Jang <doomsday@kldp.org>
 ;; You can use/modify/redistribute this freely.
 
-(eval-after-load 'google-translate
+(eval-after-load 'google-translate-default-ui
   '(progn
-     (setq google-translate-default-source-language "auto")
-     (setq google-translate-default-target-language "ko")
+     (setq google-translate-default-source-language "auto"
+           google-translate-default-target-language "ko")))
+(eval-after-load 'google-translate-smooth-ui
+  '(progn
      (setq google-translate-translation-directions-alist
            '(("en" . "ko") ("ko" . "en") ("de" . "ko") ("ko" . "de") ("ko" . "ja")))))
 (global-set-key (kbd "M-+") 'google-translate-at-point)
