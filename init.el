c243f67c3e80aa3d130678ada4a10668930a8307
Yunsik Jang <doomsday@kldp.org>
2015-11-17 00:39:20 +0900

    Fix gnuplot-mode installation on windows

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
 (or (fboundp 'erc) (el-get-bundle  erc))
 (el-get-bundle  flyspell-popup)
 (el-get-bundle  franca-idl)
-(el-get-bundle  gnuplot-mode) (or (executable-find "gnuplot") (warn "GNUPlot is not installed"))
+(el-get-bundle  gnuplot-mode :build/windows-nt (progn nil)) (or (executable-find "gnuplot") (warn "GNUPlot is not installed"))
 (el-get-bundle  google-c-style)
 (el-get-bundle  google-translate)
 (el-get-bundle  iedit)
