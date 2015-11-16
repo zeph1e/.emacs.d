7aa4eb60ec16e734909e9765db6b99f458eeb07a
Yunsik Jang <yunsik.jang@lge.com>
2015-11-16 17:25:30 +0900

    Add more configuration on google-translate

--- utils/utils-google-translate.el
+++ utils/utils-google-translate.el
@@ -6,5 +6,8 @@
 (eval-after-load 'google-translate
   '(progn
      (setq google-translate-default-source-language "auto")
-     (setq google-translate-default-target-language "ko")))
+     (setq google-translate-default-target-language "ko")
+     (setq google-translate-translation-directions-alist
+           '(("en" . "ko") ("ko" . "en") ("de" . "ko") ("ko" . "de") ("ko" . "ja")))))
 (global-set-key (kbd "M-+") 'google-translate-at-point)
+(global-set-key (kbd "C-+") 'google-translate-smooth-translate)
