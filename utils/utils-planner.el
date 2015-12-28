1d94ae7ebf274516edf51123ca4d042dcf9917ac
Yunsik Jang <yunsik.jang@lge.com>
2015-12-28 17:21:44 +0900

    Add planner

---
+++ utils/utils-planner.el
@@ -0,0 +1,13 @@
+;;; utils-planner.el
+
+;; Written by Yunsik Jang <doomsday@kldp.org>
+;; You can use/modify/redistribute this freely
+
+(setq planner-project "MyPlanner")
+(setq muse-project-alist
+      '(("MyPlanner"
+         ("~/plans"
+          :default "index"
+          :major-mode planner-mode
+          :visit-link planner-visit-link))))
+(or (file-exists-p "~/plans") (make-directory "~/plans"))
