fb4c73c000b0d9bf52a7ee0b7fc295770c33f2e0
Yunsik Jang <yunsik.jang@lge.com>
2016-01-20 18:22:25 +0900

    Make my:read-only-mode regard p/n bindings of other major/minor modes

--- utils/utils-readonly.el
+++ utils/utils-readonly.el
@@ -4,16 +4,23 @@
 ;; You can use/modify/redistribute this freely.
 
 (defvar my:read-only-mode-keymap
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "n") 'next-line)
-    (define-key map (kbd "p") 'previous-line)
-    map)
+  (make-sparse-keymap)
   "my:read-only-mode-keymap")
 
 (define-minor-mode my:read-only-mode
   "My read only mode which have no-mod-key bindings."
   :lighter " READONLY"
   :variable buffer-read-only
-  :keymap my:read-only-mode-keymap)
+  :keymap my:read-only-mode-keymap
+  ;; if there is any bind other than inserting p/n character
+  ;; this mode will not override it.
+  (define-key my:read-only-mode-keymap (kbd "p") nil)
+  (define-key my:read-only-mode-keymap (kbd "n") nil)
+  (let ((pkey (key-binding (kbd "p")))
+        (nkey (key-binding (kbd "n"))))
+    (define-key my:read-only-mode-keymap (kbd "p")
+      (and (equal pkey 'self-insert-command) 'previous-line))
+    (define-key my:read-only-mode-keymap (kbd "n")
+      (and (equal nkey 'self-insert-command) 'next-line))))
 
 (provide 'utils-readonly)
