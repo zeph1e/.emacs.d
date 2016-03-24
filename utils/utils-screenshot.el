ca59136d8fcb2dba161e6258cd155004cdefb6dd
Yunsik Jang <yunsik.jang@lge.com>
2016-03-24 10:19:00 +0900

    Customize screenshot-schemes

---
+++ utils/utils-screenshot.el
@@ -0,0 +1,8 @@
+;;; utils-screenshot.el
+
+;; Written by Yunsik Jang <doomsday@kldp.org>
+;; You can use/modify/redistribute this freely.
+
+(setq-default screenshot-schemes
+              '(("local" :dir "~/Pictures/")
+                ("current-directory :dir default-directory")))
