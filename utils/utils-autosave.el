d880cf98afee85412d8af5a4dfb92ebc484c67e9
Yunsik Jang <yunsik.jang@lge.com>
2016-04-06 11:36:38 +0900

    Make create auto save directory unless it exists

--- utils/utils-autosave.el
+++ utils/utils-autosave.el
@@ -9,4 +9,10 @@
 (setq backup-directory-alist `((".*" . ,emacs-backup-directory))
       auto-save-file-name-transforms `((".*" ,emacs-backup-directory t)))
 
+;; create auto save backup directory unless it exists
+(if (file-exists-p emacs-backup-directory)
+    (unless (file-directory-p emacs-backup-directory)
+      (warn "Auto save backup path, %s is not a directory!" emacs-backup-directory))
+  (mkdir emacs-backup-directory))
+
 (provide 'utils-autosave)
