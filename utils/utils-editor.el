a77d0761708fe0bc4d6f318a7726fdb99eda5755
Yunsik Jang <yunsik.jang@lge.com>
2016-05-18 08:48:12 +0900

    Highlight whitespaces less in text-mode

--- utils/utils-editor.el
+++ utils/utils-editor.el
@@ -82,14 +82,17 @@ minibuffer), then split the current window horizontally."
   :variable my:whitespace-mode
   (if my:whitespace-mode
       (progn
-        (cond ((or (derived-mode-p 'prog-mode)
-                   (derived-mode-p 'text-mode))
+        (cond ((derived-mode-p 'prog-mode)
                (setq-local whitespace-line-column fill-column)
                (setq-local whitespace-style
                            '(face trailing lines-tail tabs tab-mark)))
+              ((derived-mode-p 'text-mode)
+               (setq-local whitespace-line-column fill-column)
+               (setq-local whitespace-style
+                           '(face trailing tabs tab-mark)))
               (t
                (setq-local whitespace-line-column nil)
-               (setq-local whitespace-style '(face trailing tabs tab-mark))))
+               (setq-local whitespace-style '(face trailing))))
         (whitespace-mode t))
     (whitespace-mode -1)))
 
