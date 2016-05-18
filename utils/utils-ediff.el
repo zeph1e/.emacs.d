70a1d26e927bce24ac6c2760969309f51bd99331
Yunsik Jang <yunsik.jang@lge.com>
2016-05-18 18:15:09 +0900

    Make ediff handle git mergetool

--- utils/utils-ediff.el
+++ utils/utils-ediff.el
@@ -6,4 +6,50 @@
 (setq ediff-window-setup-function 'ediff-setup-windows-plain
       ediff-split-window-function 'split-window-horizontally)
 
+;; command line diff & merge with emacsclient
+(defvar my:ediff-frame nil)
+(make-variable-frame-local 'my:ediff-frame)
+
+(defun my:ediff-cmd-register-frame (&optional frame)
+  (setq my:ediff-frame (or frame (selected-frame))))
+
+(defun my:ediff-cmd-cleanup-frame ()
+  (dolist (frame (frame-list))
+    (with-selected-frame frame
+      (when (and (boundp 'my:ediff-frame)
+                 (framep my:ediff-frame)
+                 (equal frame my:ediff-frame))
+        (delete-frame my:ediff-frame)
+        (setq my:ediff-frame nil)))))
+
+(defun my:ediff-cmd (file-a file-b)
+  (add-hook 'ediff-quit-hook #'(lambda ()
+                                 (dolist (buf (list ediff-buffer-A ediff-buffer-B))
+                                   (with-current-buffer buf
+                                     (unless (buffer-modified-p)
+                                       (kill-buffer (current-buffer)))))
+                                 (my:ediff-cmd-cleanup-frame)))
+  (my:ediff-cmd-register-frame (selected-frame))
+  (ediff-files file-a file-b))
+
+(defun my:emerge-cmd (file-a file-b file-out)
+  (add-hook 'ediff-quit-hook #'(lambda ()
+                                 (dolist (buf (list ediff-buffer-A ediff-buffer-B ediff-buffer-C))
+                                   (with-current-buffer buf
+                                     (unless (buffer-modified-p)
+                                       (kill-buffer (current-buffer)))))
+                                 (my:ediff-cmd-cleanup-frame)))
+  (my:ediff-cmd-register-frame (selected-frame))
+  (ediff-merge-files file-a file-b nil file-out))
+
+(defun my:emerge-with-ancestor-cmd (file-a file-b file-out file-ancestor)
+  (add-hook 'ediff-quit-hook #'(lambda ()
+                                 (dolist (buf (list ediff-buffer-A ediff-buffer-B ediff-buffer-C))
+                                   (with-current-buffer buf
+                                     (unless (buffer-modified-p)
+                                       (kill-buffer (current-buffer)))))
+                                 (my:ediff-cmd-cleanup-frame)))
+  (my:ediff-cmd-register-frame (selected-frame))
+  (ediff-merge-files-with-ancestor file-a file-b file-ancestor nil file-out))
+
 (provide 'utils-ediff)
