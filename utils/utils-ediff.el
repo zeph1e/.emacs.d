103e9484cf7cdbaa28d077c892ad25f54a2ce268
Yunsik Jang <yunsik.jang@lge.com>
2016-05-26 15:19:23 +0900

    Fix ediff & edit script to cope with several situations

--- utils/utils-ediff.el
+++ utils/utils-ediff.el
@@ -10,6 +10,27 @@
 (defvar my:ediff-frame nil)
 (make-variable-frame-local 'my:ediff-frame)
 
+(defun my:command-line-diff (switch)
+  (let* ((file-a (pop command-line-args-left))
+         (file-b (pop command-line-args-left)))
+    (add-hook 'ediff-quit-hook 'kill-emacs)
+    (ediff-files file-a file-b)))
+
+(defun my:command-line-merge (switch)
+  (let* ((file-a (pop command-line-args-left))
+         (file-b (pop command-line-args-left))
+         (file-out (pop command-line-args-left))
+         (file-ancestor (pop command-line-args-left)))
+    (add-hook 'ediff-quit-hook 'kill-emacs)
+    (if (and file-ancestor
+             (file-exists-p file-ancestor)
+             (file-readable-p file-ancestor))
+        (ediff-merge-files-with-ancestor file-a file-b file-ancestor nil file-out)
+      (ediff-merge-files file-a file-b nil file-out))))
+
+(add-to-list 'command-switch-alist '("diff" . my:command-line-diff))
+(add-to-list 'command-switch-alist '("merge" . my:command-line-merge))
+
 (defun my:ediff-cmd-register-frame (&optional frame)
   (setq my:ediff-frame (or frame (selected-frame))))
 
@@ -27,14 +48,13 @@
   (my:ediff-cmd-register-frame (selected-frame))
   (ediff-files file-a file-b))
 
-(defun my:emerge-cmd (file-a file-b file-out)
+(defun my:emerge-cmd (file-a file-b file-out &optional file-ancestor)
   (add-hook 'ediff-quit-hook #'my:ediff-cmd-cleanup-frame)
   (my:ediff-cmd-register-frame (selected-frame))
-  (ediff-merge-files file-a file-b nil file-out))
-
-(defun my:emerge-with-ancestor-cmd (file-a file-b file-out file-ancestor)
-  (add-hook 'ediff-quit-hook #'my:ediff-cmd-cleanup-frame)
-  (my:ediff-cmd-register-frame (selected-frame))
-  (ediff-merge-files-with-ancestor file-a file-b file-ancestor nil file-out))
+  (if (and file-ancestor
+           (file-exists-p file-ancestor)
+           (file-readable-p file-ancestor))
+      (ediff-merge-files-with-ancestor file-a file-b file-ancestor nil file-out)
+    (ediff-merge-files file-a file-b nil file-out)))
 
 (provide 'utils-ediff)
