3ee5a6aad68101ae14f80635909a3bf7b08862dc
Yunsik Jang <doomsday@kldp.org>
2016-05-27 21:44:57 +0900

    Fix error in previous commit

--- utils/utils-aliases.el
+++ utils/utils-aliases.el
@@ -3,7 +3,7 @@
 ;; Written by Yunsik Jang <doomsday@kldp.org>
 ;; You can use/modify/redistribute this freely
 
-(defalias 'ke! #'save-buffers-kill-emacs)
+(defalias 'ke! #'save-buffers-kill-emacs
   "Save buffers and kill emacs.")
 
 (provide 'utils-aliases)
