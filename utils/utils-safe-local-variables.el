5d728a1b24ac4266c8c733de32a28743403aacd5
Yunsik Jang <yunsik.jang@lge.com>
2016-05-27 16:36:24 +0900

    Make a place to add safe-local-variable-value

---
+++ utils/utils-safe-local-variables.el
@@ -0,0 +1,17 @@
+;;; utils-safe-local-variables.el
+
+;; Written by Yunsik Jang <doomsday@kldp.org>
+;; You can use/modify/redistribute this freely
+
+;; I hate emacs ask about following variables are ok
+;; while I don't want put cutomized variables part into
+;; init.el
+
+(defconst my:safe-local-variables
+  '((cscope-option-do-not-update-database . t)
+    ))
+
+(setq safe-local-variable-values
+      (append safe-local-variable-values my:safe-local-variables))
+
+(provide 'utils-safe-local-variables)
