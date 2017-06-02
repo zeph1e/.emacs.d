cefea1fe34de91929e5a7f5dfafc86cf4e142c40
Yunsik Jang <yunsik.jang@lge.com>
2017-06-02 15:54:53 +0900

    Make interpreter of python be version 3

--- utils/utils-python.el
+++ utils/utils-python.el
@@ -5,6 +5,8 @@
 
 (setq anaconda-mode-installation-directory
       (el-get-package-directory 'anaconda-mode))
+(setq python-shell-interpreter (or (executable-find "python3")
+                                   "python"))
 
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
