2e05121ada5a9ae098a6524505e91fc8c3ec28f2
Yunsik Jang <yunsik.jang@lge.com>
2017-06-29 18:08:14 +0900

    Make fci-mode be enabled in buffer in foreground/wide-enough window

--- utils/utils-editor.el
+++ utils/utils-editor.el
@@ -99,27 +99,67 @@ minibuffer), then split the current window horizontally."
 
 ;; Disable fci-mode if the window is smaller than fill-column
 (defvar my:fci-mode-suppressed nil)
-(make-local-variable 'my:fci-mode-suppressed)
-
-(defvar my:fci-selected-frame nil
-  "Selected frame to trace changes in frame selection.")
 
 (defvar my:fci-selected-window nil
   "Selected window to trace changes in window selection.")
 
-(defun my:suppress-fci-mode-in-narrow-window ()
-  (let* ((window (selected-window))
-         (width (window-body-width window)))
-    (with-current-buffer (window-buffer window)
-      (if (and (boundp fci-mode)
-               fci-mode)
-          (when (and (null fci-handle-truncate-lines)
-                     (<= width (1+ fill-column)))
-            (setq my:fci-mode-suppressed t)
-            (turn-off-fci-mode))
-        (when my:fci-mode-suppressed
-          (setq my:fci-mode-suppressed nil)
-          (turn-on-fci-mode))))))
+(defun my:fci-narrow-window-p (window-or-frame)
+  (let* ((window (cond ((framep window-or-frame)
+                        (with-selected-frame window-or-frame
+                          (selected-window)))
+                       ((windowp window-or-frame) window-or-frame)
+                       ((null window-or-frame)
+                        (with-selected-frame (selected-frame)
+                          (selected-window)))
+                       (t (error (format "Invalid type %s is given"
+                                         (type-of window-or-frame))))))
+         (width (window-body-width window))
+         (ruler (with-current-buffer (window-buffer window)
+                  (or fci-rule-column
+                      fill-column))))
+    (<= width (1+ ruler))))
+
+(defun my:fci-activate (&optional buffer-or-name)
+  (with-current-buffer (or (and buffer-or-name
+                                (get-buffer buffer-or-name))
+                           (current-buffer))
+    (when (and my:fci-mode-suppressed
+               (not (delete nil (mapcar ; all of windows are wide enough
+                                 #'(lambda (w)
+                                     (my:fci-narrow-window-p w))
+                                 (get-buffer-window-list
+                                  (current-buffer) nil t)))))
+      (turn-on-fci-mode)
+      (setq-local my:fci-mode-suppressed nil))))
+
+(defun my:fci-suppress (&optional buffer-or-name)
+  (with-current-buffer (or (and buffer-or-name
+                                (get-buffer buffer-or-name))
+                           (current-buffer))
+    (when fci-mode
+      (setq-local my:fci-mode-suppressed t)
+      (turn-off-fci-mode))))
+
+(defun my:fci-activate-focused (&optional frame-or-window)
+  (let* ((frame (cond ((framep frame-or-window) frame-or-window)
+                      ((windowp frame-or-window) (window-frame frame-or-window))
+                      (t (selected-frame))))
+         (window (cond ((windowp frame-or-window) frame-or-window)
+                       (t (with-selected-frame frame
+                            (selected-window)))))
+         (buffer-to-clear  (unless (or (null my:fci-selected-window)
+                                       (eq my:fci-selected-window window))
+                             (window-buffer my:fci-selected-window))))
+    ;; clear fci-overlays in background buffer
+    (when (and (bufferp buffer-to-clear)
+               (not (eq buffer-to-clear (window-buffer (selected-window)))))
+      (with-current-buffer buffer-to-clear
+        (my:fci-suppress)))
+    (if (and (eq my:fci-selected-window window)
+             (my:fci-narrow-window-p window))
+        (my:fci-suppress)
+      (my:fci-activate))
+    (setq my:fci-selected-window window)))
 
 ;; Setting fci-mode variables
 (with-eval-after-load 'fill-column-indicator
@@ -128,7 +168,11 @@ minibuffer), then split the current window horizontally."
   (setq fci-rule-column nil)
   (setq fci-rule-width 1)
   (add-hook 'window-configuration-change-hook
-            'my:suppress-fci-mode-in-narrow-window))
+            'my:fci-activate-focused)
+  (add-hook 'buffer-list-update-hook
+            'my:fci-activate-focused)
+  (add-to-list 'window-size-change-functions
+               'my:fci-activate-focused))
 
 ;; share clipboard even in terminal
 ;; http://blog.binchen.org/posts/copypaste-in-emacs.html
