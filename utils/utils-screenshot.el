74cc5e69e553dcbc2357bc32b8ae372e4e3a2986
Yunsik Jang <doomsday@kldp.org>
2017-07-05 00:31:02 +0900

    Fix screenshot to handle new imagemagick executable

--- utils/utils-screenshot.el
+++ utils/utils-screenshot.el
@@ -7,4 +7,16 @@
               '(("local" :dir "~/Pictures/")
                 ("current-directory :dir default-directory")))
 
+(defun my:screenshot-do-import (filename)
+  "Run ImageMagick import command. Recent version of ImageMagick
+changed to whole functionalities in only a command `magick'."
+  (let ((magick (executable-find "magick"))
+        (import (executable-find "import")))
+    (if magick
+        (call-process magick nil nil nil "import" filename)
+      (call-process import nil nil nil filename))))
+
+(with-eval-after-load 'screenshot
+  (defalias 'screenshot-do-import 'my:screenshot-do-import))
+
 (provide 'utils-screenshot)
