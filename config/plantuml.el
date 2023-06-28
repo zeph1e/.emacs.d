87fd2c85ec5554e9e84294249a3fbd6a69d05a6d
Yunsik Jang <yunsik.jang@lge.com>
2023-06-28 10:17:03 +0900

    Add xcscope config

--- config/plantuml.el
+++ config/plantuml.el
@@ -2,6 +2,7 @@
   :custom
   (plantuml-default-exec-mode 'jar)
   (plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
+  (plantuml-indent-level 4)
   :config
   (setq-default plantuml-output-type "png")
   :init
