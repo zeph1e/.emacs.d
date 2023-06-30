9abec99e3203c0f51970b08f05fc2e93d02e6a89
Yunsik Jang <yunsik.jang@lge.com>
2023-06-30 16:41:42 +0900

    Add dired configuration

---
+++ config/dired.el
@@ -0,0 +1,17 @@
+(use-package dired
+  :ensure nil
+  :config
+  (defvar my:view-file-opener
+    (eval-when-compile
+      (or (executable-find "explorer.exe") (executable-find "xdg-open"))))
+
+  (defun my:view-file-external (file)
+    (interactive (list (convert-standard-filename (expand-file-name (dired-file-name-at-point)))))
+    (when (file-remote-p file)
+      (let ((target-file (make-temp-file "view-file" nil (concat "-" (file-name-nondirectory file)))))
+	(tramp-compat-copy-file file target-file t)
+	(setq file target-file)))
+    (and my:view-file-opener (call-process my:view-file-opener nil 0 nil file)))
+  :bind
+  (("C-x C-j" . dired-jump)
+   ("V" . my:view-file-external)))
