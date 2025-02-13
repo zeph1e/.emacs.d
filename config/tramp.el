b981b8391562bca7a18a5c9136e1bf6205c361a3
Yunsik Jang <yunsik.jang@lge.com>
2025-02-13 19:57:47 +0900

    Add user local directory to tramp remote path

--- config/tramp.el
+++ config/tramp.el
@@ -1,5 +1,7 @@
 ;;-*- mode: emacs-lisp; -*-
 (use-package tramp
+  :config
+  (add-to-list 'tramp-remote-path (concat (getenv "HOME") "/.local/bin"))
   :custom
   (tramp-default-method "ssh")
   (enable-remote-dir-locals t))
