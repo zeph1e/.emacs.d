0fef5fa8cc1fb8bc12c254dbc83b2a2d6963cc5c
Yunsik Jang <z3ph1e@gmail.com>
2025-03-06 00:59:27 +0900

    Add projectile.el

---
+++ config/projectile.el
@@ -0,0 +1,11 @@
+;;-*- mode: emacs-lisp; -*-
+(use-package projectile
+  :init
+  (projectile-global-mode)
+  :custom
+  (projectile-enable-caching t)
+  (projectile-file-exists-remote-cache-expire (* 7 24 60 60)) ; a week
+  (projectile-file-exists-local-cache-expire (* 7 24 60 60)) ; a week
+  (projectile-completion-system 'helm)
+  (projectile-switch-project-action 'projectile-find-file-dwim)
+  (projectile-switch-project 'helm-projectile))
