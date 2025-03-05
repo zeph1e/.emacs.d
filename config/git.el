54c3ea1497594d840b45a4030adac44448816ca6
Yunsik Jang <z3ph1e@gmail.com>
2025-03-06 01:19:10 +0900

    Add more key bindings of magit

--- config/git.el
+++ config/git.el
@@ -4,4 +4,6 @@
   (:map my:global-key-map
    ("C-x RET C-s" . magit)
    ("C-x RET C-h" . magit-log-head)
-   ("C-x RET C-b" . magit-blame)))
+   ("C-x RET C-b" . magit-blame)
+   ("C-x RET C-f" . magit-find-file)
+   ("C-x RET C-l" . magit-log-buffer-file)))
