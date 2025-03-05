fb963a66e566069bfad1490cf5deebd40c59b1fb
Yunsik Jang <z3ph1e@gmail.com>
2025-03-06 00:55:05 +0900

    Migrate old helm key bindings

--- config/helm.el
+++ config/helm.el
@@ -1,6 +1,14 @@
 ;;-*- mode: emacs-lisp; -*-
 (use-package helm
   :demand t
+  :config
+  (require 'helm-files)
+  (defun my:helm-find-files-backward-dwim ()
+    "Backspace goes to the upper folder if you're not in a filename."
+    (interactive)
+    (if (looking-back "/" 1)
+        (call-interactively 'helm-find-files-up-one-level)
+      (delete-backward-char 1)))
   :bind
   (:map my:global-key-map
    ("M-x"       . helm-M-x)
@@ -18,8 +26,12 @@
    ("<tab>" . helm-execute-persistent-action)
    ("C-i"   . helm-execute-persistent-action)
    ("C-j"   . helm-select-action)
-   ("M-j"   . helm-next-source)
-   ("M-k"   . helm-previous-source))
+   ("M-n"   . helm-next-source)
+   ("M-p"   . helm-previous-source)
+   :map helm-read-file-map
+   ("<backspace>" . my:helm-find-files-backward-dwim)
+   :map helm-find-files-map
+   ("<backspace>" . my:helm-find-files-backward-dwim))
   :custom
   (helm-split-window-preferred-function 'helm-split-window-default-fn)
   (helm-autoresize-mode 1)
@@ -46,3 +58,15 @@
   :init
   (helm-projectile-on)
   :after (helm projectile))
+
+(use-package helm-ag
+  :bind
+  (:map helm-ag-map
+   ("M-n" . helm-ag--next-file)
+   ("M-p" . helm-ag--previous-file))
+  (:map my:global-key-map
+   ("C-M-r" . helm-ag))
+  :custom
+  (helm-ag-insert-at-point 'thing-at-point)
+  (helm-ag-fuzzy-match t)
+  :after (helm))
