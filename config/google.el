6c2216be085a4acfcb99fb666cc48613c79b4473
Yunsik Jang <z3ph1e@gmail.com>
2025-03-26 03:39:56 +0900

    Add target language detection in google translate

--- config/google.el
+++ config/google.el
@@ -16,7 +16,48 @@
   (eval-after-load 'google-translate-smooth-ui
     '(setq google-translate-translation-directions-alist
              '(("en" . "ko") ("ko" . "en") ("de" . "ko") ("ko" . "de")
-               ("en" . "de") ("de" . "en") ("ko" . "ja"))))
+               ("en" . "de") ("de" . "en") ("ja" . "ko") ("ko" . "ja"))))
+  :config
+  ;; Detect source language and select preferred target language
+  (defconst my:google-translate-unicode-range
+    '(("ko" . ((#xAC00 . #xD3A3)))
+      ("ja" . ((#x3041 . #x30F6)))
+      ("de" . (#x00C4 #x00D6 #x00DC #x00DF #x00E4 #x00F6 #x00FC))))
+
+  (defun my:google-translate-translate (args)
+    (let ((source-lang (nth 0 args))
+          (target-lang (nth 1 args))
+          (text (nth 2 args)))
+      (catch 'break
+        (dotimes (l (length my:google-translate-unicode-range))
+          (let ((lang (car (nth l my:google-translate-unicode-range)))
+                (range (cdr (nth l my:google-translate-unicode-range))))
+            (dotimes (i (length text))
+              (let ((c (aref text i)))
+                (when (mapcan (lambda (elem)
+                                (cond ((numberp elem)
+                                       (= elem c))
+                                      ((listp elem)
+                                       (and (<= (car elem) c)
+                                            (>= (cdr elem) c)))
+                                      (t nil)))
+                              range)
+                  (setq source-lang lang)
+                  (throw 'break nil)))))))
+      (when (or (string= source-lang target-lang)
+                (string= target-lang "auto"))
+        (catch 'break
+          (dotimes (i (length google-translate-translation-directions-alist))
+            (let ((dir (nth i google-translate-translation-directions-alist)))
+              (when (string= (car dir) source-lang)
+                (setq target-lang (cdr dir))
+                (throw 'break nil))))))
+      (setf (nth 0 args) source-lang
+            (nth 1 args) target-lang)
+      args))
+
+  (advice-add 'google-translate-translate
+              :filter-args 'my:google-translate-translate)
   :bind
   (:map my:global-key-map
    ("M-+" . google-translate-at-point)
