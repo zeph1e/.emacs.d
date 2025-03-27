c6a6a7211d6bc0780a660f3ec8cba917f474e4a5
Yunsik Jang <z3ph1e@gmail.com>
2025-03-27 22:51:35 +0900

    Add flyspell config

---
+++ config/flyspell.el
@@ -0,0 +1,16 @@
+(use-package flyspell
+  :ensure-system-package
+  (ispell . "sudo apt install -y ispell")
+  :config
+  (defun my:toggle-flyspell-mode ()
+    (interactive)
+    (if flyspell-mode
+        (flyspell-mode -1)
+      (if (derived-mode-p 'prog-mode)
+          (flyspell-prog-mode)
+        (flyspell-mode))
+      (when (called-interactively-p)
+        (flyspell-buffer))))
+  :bind
+  (:map my:global-key-map
+   ("<f8>" . my:toggle-flyspell-mode)))
