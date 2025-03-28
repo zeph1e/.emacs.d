b9909bcbcb84bf24c38aa8b1aa9465dfcdb775e5
Yunsik Jang <yunsik.jang@lge.com>
2025-03-28 13:11:59 +0900

    Add web-beautify config

--- config/web.el
+++ config/web.el
@@ -13,3 +13,22 @@
   (web-mode-enable-auto-pairing t)
   (web-mode-enable-auto-indentation t)
   (web-mode-engines-alist '(("php" . "\\.php\\'"))))
+
+(use-package web-beautify
+  :init
+  (eval-after-load 'js
+    '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
+  (eval-after-load 'json-mode
+    '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+  (eval-after-load 'sgml-mode
+    '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+  (eval-after-load 'web-mode
+    '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
+  (eval-after-load 'css-mode
+    '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+
+  :config
+  (setq web-beautify-args '("-s" "2" "-f" "-"))
+  :ensure-system-package
+  ((npm . "sudo apt install -y npm")
+   (js-beautify . "npm -g install js-beautify")))
