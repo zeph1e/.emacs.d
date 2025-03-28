23fab9a471ea4053e09222c5c4e72754b018cd97
Yunsik Jang <yunsik.jang@lge.com>
2025-03-28 11:52:12 +0900

    Misc change for tramp

--- config/tramp.el
+++ config/tramp.el
@@ -3,5 +3,6 @@
   :config
   (add-to-list 'tramp-remote-path (concat (getenv "HOME") "/.local/bin"))
   :custom
-  (tramp-default-method "ssh")
+  (tramp-default-method
+   (ignore-errors (file-name-nondirectory (executable-find "ssh"))))
   (enable-remote-dir-locals t))
