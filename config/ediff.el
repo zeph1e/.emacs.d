28ae32469e2c332766ef9f11fdfeb77528f83306
Yunsik Jang <yunsik.jang@lge.com>
2025-03-31 13:54:53 +0900

    Fit to 80 cols

--- config/ediff.el
+++ config/ediff.el
@@ -10,8 +10,12 @@
 ;; ediff help functions
 (defun my:ediff-copy-both-to-C ()
   (interactive)
-  (ediff-copy-diff ediff-current-difference nil 'C nil
-                   (concat
-                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
-                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
-(add-hook 'ediff-keymap-setup-hook (lambda () (define-key ediff-mode-map "c" 'my:ediff-copy-both-to-C)))
+  (ediff-copy-diff
+   ediff-current-difference nil 'C nil
+   (concat
+    (ediff-get-region-contents
+     ediff-current-difference 'A ediff-control-buffer)
+    (ediff-get-region-contents
+     ediff-current-difference 'B ediff-control-buffer))))
+(add-hook 'ediff-keymap-setup-hook
+          (lambda () (define-key ediff-mode-map "c" 'my:ediff-copy-both-to-C)))
