4853f6a59cdd03d1773b6258fa5974c1ec481b51
Yunsik Jang <yunsik.jang@lge.com>
2025-04-01 15:03:45 +0900

    Fix lsp config

--- config/lsp.el
+++ config/lsp.el
@@ -4,20 +4,21 @@
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
-  :init
-  ;; to get lsp-mode going with xtensa
-  (setq lsp-clients-clangd-executable "clangd")
-  (setq lsp_clients-clangd-args
-        '("--query-driver=/**/bin/xtensa-esp32-elf-*"
-          "--background-index"
-          "--header-insertion=iwyu"
-          "-j=4"))
   :hook
   (c-mode . lsp)
   (c++-mode . lsp)
   (js-mode . lsp-deferred)
   (typescript-mode . lsp-deferred)
   (python-mode . lsp-deferred)
-  (lsp-mode . lsp-enable-which-key-integration))
+  (lsp-mode . lsp-enable-which-key-integration)
+  :custom
+  ((lsp-keymap-prefix "C-c C-l")
+   ;; to get lsp-mode going with xtensa
+   (lsp-clients-clangd-executable (executable-find "clangd"))
+   (lsp-clients-clangd-args
+    '("--query-driver=/**/bin/xtensa-esp32-elf-*"
+      "--background-index"
+      "--header-insertion=iwyu"
+      "-j=4"))))
 
 (use-package lsp-ui)
