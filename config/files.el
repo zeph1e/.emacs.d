6fbd6dca1ed75aa97981a9130b4d17a498a8ae2f
Yunsik Jang <z3ph1e@gmail.com>
2025-04-02 20:51:56 +0900

    Migrate that showing buffer changes in saving actions

--- config/files.el
+++ config/files.el
@@ -25,40 +25,36 @@
               null-device)
             buffer nil 'noasync)))
 
-  (defun my:revert-buffer (&optional buffer-or-name)
-    "Refreshes this buffer from its respective file."
-    (interactive)
-    (let ((buffer (get-buffer (or buffer-or-name (current-buffer))))
-          choice)
-      (when (and (bufferp buffer)
-                 (buffer-file-name buffer)
-                 (file-exists-p (buffer-file-name buffer)))
-        (with-current-buffer buffer
-          (setq choice
-                (or (unless (buffer-modified-p) ?d)
-                    (save-window-excursion
-                      (my:display-buffer-modification buffer)
-                      (read-char-choice
-                       (format
-                        "Buffer %s modified; (d)iscard (s)ave or (q)uit? "
-                        (buffer-name buffer))
-                       '(?d ?q ?s)))))
-          (cond ((equal choice ?d)
-                 (revert-buffer t t t)
-                 (message "Refreshed buffer: %s." (buffer-name buffer)))
-                ((equal choice ?s)
-                 (save-buffer)))))))
+  (defun my:revert-buffer (ignore-auto noconfirm)
+    "My custom function for revert-buffer which shows the differences.
+Runs revert-buffer--default internally"
+    (cl-letf (((symbol-function 'yes-or-no-p)
+               #'(lambda (prompt)
+                   (save-window-excursion
+                     (my:display-buffer-modification (current-buffer))
+                     (let* ((buffer (current-buffer))
+                           (choice
+                            (read-char-choice
+                             (format
+                              "Buffer %s modified; (d)iscard (s)ave or (q)uit? "
+                              (buffer-name buffer))
+                             '(?d ?q ?s))))
+                       (cond ((equal choice ?d)
+                              (message "Refreshed buffer: %s."
+                                       (buffer-name buffer)) t)
+                             ((equal choice ?s) (save-buffer) nil)))))))
+      (apply #'revert-buffer--default `(,ignore-auto ,noconfirm))))
 
   (defun my:revert-all-buffers ()
     "Refreshes all open buffers from their respective files."
     (interactive)
     (dolist (buf (buffer-list))
       (with-current-buffer buf
-        (my:revert-buffer buf)))
+        (revert-buffer buf)))
     (message "Refreshed all opened files."))
 
   (defadvice kill-buffer (around my:kill-buffer (&optional buffer-or-name))
-    "Adviced kill buffer to show diff with original file to verify the changes."
+    "Advised kill buffer to show diff with original file to verify the changes."
     (with-current-buffer (or buffer-or-name (current-buffer))
       (if (and (buffer-live-p (current-buffer))
                (buffer-modified-p)
@@ -72,23 +68,43 @@
         ad-do-it)))
   (ad-activate 'kill-buffer)
 
+  (defadvice recover-file (around my:recover-file (file))
+    "Advised recover-file to diff buffer with auto-save file."
+    (interactive "FRecover file: ")
+    (cl-letf* ((file (expand-file-name file))
+               (file-buffer (find-buffer-visiting file))
+               (file-name (let ((buffer-filen-name file))
+                            (make-auto-save-file-name)))
+               (diff-switches "-urN")
+               ((symbol-function 'read-answer)
+                #'(lambda (question answer)
+                    (display-buffer file-buffer '(display-buffer-same-window))
+                    (delete-other-windows)
+                    (diff file-buffer file-name nil 'noasync)
+                    (if (y-or-n-p question) "yes" "no"))))
+      (save-window-excursion
+        ad-do-it)))
+  (ad-activate 'recover-file)
 
-  ;; (defun my:recover-file (file)
-  ;;   (interactive "FRecover file: ")
-  ;;   (when (null file)
-  ;;     (error "Visiting buffer %S doesn't have a name of file."
-  ;;            (current-buffer)))
-  ;;   (setq file (expand-file-name file))
-  ;;   (save-window-excursion
-  ;;     (let* ((file-buffer (find-buffer-visiting file))
-  ;;            (file-name (let ((buffer-file-name file))
-  ;;                         (make-auto-save-file-name)))
-  ;;            (diff-switches "-urN"))
-  ;;       (display-buffer file-buffer '(display-buffer-same-window))
-  ;;       (delete-other-windows)
-  ;;       (diff file-buffer file-name nil 'noasync)
-  ;;       (recover-file file))))
+  (defadvice save-some-buffers
+      (around my:save-some-buffers (&optional arg pred))
+    "Advised save-some-buffers to show what is changed in the buffers."
+    (cl-letf* ((orig-map-y-or-n-p (symbol-function 'map-y-or-n-p))
+               ((symbol-function 'map-y-or-n-p)
+               #'(lambda (prompter actor list &optional help action-alist
+                                   no-cursor-in-echo-area)
+                   (funcall orig-map-y-or-n-p
+                            #'(lambda (buffer)
+                              (my:display-buffer-modification buffer)
+                              (funcall prompter buffer))
+                            actor list help action-alist
+                            no-cursor-in-echo-area))))
+      ad-do-it))
+  (ad-activate 'save-some-buffers)
+
   :bind
   (:map my:global-key-map
-   ("<f5>" . my:revert-buffer)
-   ("C-<f5>" . my:revert-all-buffers)))
+   ("<f5>" . revert-buffer)
+   ("C-<f5>" . my:revert-all-buffers))
+  :custom
+  (revert-buffer-function #'my:revert-buffer))
