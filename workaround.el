8e0840901d19f5a9f76983c887f809b324246f1f
Yunsik Jang <z3ph1e@gmail.com>
2025-04-03 23:23:57 +0900

    Remove redraw-modeline workaround for nyan-mode
    nyan-mode has been fixed to use force-mode-line-update

--- workaround.el
+++ workaround.el
@@ -1,7 +1,4 @@
 ;;-*- mode: emacs-lisp; -*-
-(defalias 'redraw-modeline 'force-mode-line-update
-  "Alias, redraw-modeline is removed around version 29")
-
 ;; To fix issue in dictionaries-common:
 ;; https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=968955
 (setq ispell-menu-map-needed t)
