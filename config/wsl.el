650a9321b647defd51265457ceb84c80473371ed
Yunsik Jang <z3ph1e@gmail.com>
2025-04-06 00:53:16 +0900

    Use more easy way to launch windows browser on WSL

--- config/wsl.el
+++ config/wsl.el
@@ -15,73 +15,22 @@
 
 (use-package browse-url
   :pin manual
-  :init
-  ;; This is to launch URLs in a web browser on Windows, not in WSL, even if
-  ;; there's a installed web browser in WSL.
-  (defvar my:wsl-browser-command
+  :config
+  (defun my:browse-url-wslview (url &optional _new-window)
+    "Ask the default WWW browser in Windows to load URL through wslview
+in WSL. Default to the URL around or before point.
+The optional argument NEW-WINDOW is not used."
     (when my:wslp
-      (let* ((reg (executable-find "reg.exe"))
-             (coding-system-for-read (default-value 'buffer-file-coding-system))
-             (user-choice-key
-              (concat "HKCU\\SOFTWARE\\Microsoft\\Windows\\Shell\\"
-                      "Associations\\UrlAssociations\\http\\UserChoice"))
-             (command-key-format
-              "HKLM\\SOFTWARE\\Classes\\%s\\shell\\open\\command")
-             (prog-id
-              (with-temp-buffer
-                ;; This queries the program ID of default web browser to open
-                ;; a url in http. `prog-id' may contains "ChromeHTML" for
-                ;; Google Chrome as well as "microsoft-edge" for Microsoft Edge.
-                ;; I have not tested for Firefox unfortunately.
-                (when (= 0 (call-process reg nil (current-buffer) nil
-                                         "query" user-choice-key "/v" "ProgId"))
-                  (goto-char (point-min))
-                  (when (re-search-forward "ProgId\\s-+REG_SZ\\s-+\\(.*\\)"
-                                           nil t)
-                    (string-trim
-                     (decode-coding-string (match-string 1) 'utf-8)))))))
-        (when (and prog-id (stringp prog-id))
-          ;; This queries the program path and the list of arguments to pass.
-          ;; Since it is in Windows style path, we need to convert it to one
-          ;; can be accessed in unix-like path.
-          (let ((command-key (format command-key-format prog-id)))
-            (with-temp-buffer
-              (when (= 0 (call-process reg nil (current-buffer) nil
-                                       "query" command-key "/ve"))
-                (goto-char (point-min))
-                (when (re-search-forward "REG_SZ\\s-+\\(.*\\)" nil t)
-                  (let* ((input-string (string-trim
-                                        (decode-coding-string
-                                         (match-string 1) 'utf-8)))
-                         (prog-command
-                          (let ((start 0)
-                                (result '()))
-                            (while (string-match
-                                    "\"\\([^\"]+\\)\"\\|\\([^ ]+\\)"
-                                    input-string start)
-                              (setq start (match-end 0))
-                              (push (or (match-string 1 input-string)
-                                        (match-string 2 input-string))
-                                    result))
-                            (nreverse result))))
-                    (setf (car prog-command)
-                          (concat "/mnt/"
-                                  (downcase (substring (car prog-command) 0 1))
-                                  (replace-regexp-in-string
-                                   "\\\\" "/"
-                                   (substring (car prog-command) 2))))
-                    prog-command)))))))))
+      (let ((wslview (executable-find "wslview")))
+        (if wslview
+            (call-process wslview nil (current-buffer) nil url)
+          (error "Unable to find wslview")))))
 
-  (setq browse-url-browser-function
-        #'(lambda (url &rest args)
-            (if my:wsl-browser-command
-                (apply #'call-process
-                       (cons (car my:wsl-browser-command)
-                             (append '(nil 0 nil)
-                                     (mapcar (lambda (param)
-                                               (if (string= "%1" param)
-                                                   url
-                                                 param))
-                                             (cdr my:wsl-browser-command)))))
-                       (funcall (default-value 'browse-url-browser-function)
-                                url args)))))
+  (function-put 'my:browse-url-wslview 'browse-url-browser-kind 'external)
+
+  (defun my:browse-url-default-browser (url &rest args)
+    (if my:wslp
+        (my:browse-url-wslview url args)
+      (funcall (default-value 'browse-url-browser-function) url args)))
+  :custom
+  (browse-url-browser-function #'my:browse-url-default-browser))
