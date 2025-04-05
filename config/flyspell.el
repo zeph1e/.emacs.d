986cb574efa5047276b80396f2768920a58992fc
Yunsik Jang <z3ph1e@gmail.com>
2025-04-06 00:53:40 +0900

    Make flyspell ignore url

--- config/flyspell.el
+++ config/flyspell.el
@@ -2,6 +2,13 @@
   :ensure-system-package
   (ispell . "sudo apt install -y ispell")
   :config
+  ;; To make flyspell ignore urls
+  ;; https://emacs.stackexchange.com/q/5415
+  (defun my:flyspell-ignore-url ()
+    "Function used for `flyspell-generic-check-word-predicate'
+to ignore url stuff"
+    (not (thing-at-point 'url)))
+
   (defun my:toggle-flyspell-mode ()
     (interactive)
     (if flyspell-mode
@@ -9,6 +16,7 @@
       (if (derived-mode-p 'prog-mode)
           (flyspell-prog-mode)
         (flyspell-mode))
+      (put major-mode 'flyspell-mode-predicate 'my:flyspell-ignore-url)
       (when (called-interactively-p)
         (flyspell-buffer))))
   :bind
