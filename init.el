0eb850fe9fb119c52317f19a9e6a83f22a2bb8ed
Yunsik Jang <z3ph1e@gmail.com>
2025-04-06 04:15:57 +0900

    Rearrange init.el

--- init.el
+++ init.el
@@ -55,7 +55,7 @@
                                (t 'tty)))))))
 
 ;; customization settings
-(setq custom-file (concat user-emacs-directory "custom.el"))
+(setq custom-file (locate-user-emacs-file "custom.el"))
 (unless (file-exists-p custom-file)
   (with-temp-buffer
     (insert ";;; custom.el --- local customization\n")
@@ -72,11 +72,12 @@
         lock-file-name-transforms `((".*" ,lock-files-dir t))
         tramp-backup-directory-alist `((".*" . ,backups-dir))
         tramp-auto-save-directory auto-saves-dir)
-  (dolist (dir (list backups-dir auto-saves-dir lock-files-dir))
-    (if (file-exists-p dir)
-        (unless (file-directory-p dir)
-          (warn "backup path, %s is not a directory!" dir))
-      (mkdir dir))))
+  (mapc (lambda (dir)
+          (if (file-exists-p dir)
+              (unless (file-directory-p dir)
+                (warn "backup path, %s is not a directory!" dir))
+            (mkdir dir)))
+        (list backups-dir auto-saves-dir lock-files-dir)))
 
 (setq make-backup-files t
       backup-by-copying t
@@ -86,16 +87,42 @@
       kept-old-versions 2)
 
 ;; load workaround
-(load (concat user-emacs-directory "workaround.el"))
+(load (locate-user-emacs-file "workaround.el"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; package & use-package initialization
+(require 'package)
+;; Add various package archives
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa" . "https://melpa.org/packages/")))
+;; Initialise packages
+(package-initialize)
+
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; use-package is landed as a built-in package into emacs 29
+;; In this case we don't need to add a load path
+(when (< (string-to-number emacs-version) 29)
+  (add-to-list 'load-path
+               (package-desc-dir
+                (car (cdr (assq 'use-package package-alist))))))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 ;; add local package path to load path
-(let ((default-directory (concat user-emacs-directory "plugins")))
-  (normal-top-level-add-subdirs-to-load-path)
+(let ((default-directory (locate-user-emacs-file "plugins")))
+  (normal-top-level-add-subdirs-to-load-path) ; add sub-directories to load-path
   (mapc (lambda (dir)
           (let ((installed-flag (concat dir "/.installed")))
             (when (and (file-directory-p dir)
                        (not (string-prefix-p "." dir)))
               (unless (file-exists-p installed-flag)
+                ;; Writes an autoloads file and byte-compiles the scripts
                 (mapc (lambda (file)
                         (when (and (not (file-symlink-p file))
                                    (not (file-directory-p file))
@@ -105,36 +132,10 @@
                 (make-directory-autoloads
                  dir (concat dir "/" (directory-file-name dir) "-autoloads.el"))
                 (make-empty-file installed-flag))
+              ;; load autoloads files
               (require (intern (concat (directory-file-name dir)
                                        "-autoloads"))))))
           (directory-files default-directory)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; package & use-package initialization
-(require 'package)
-;; Add various package archives
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "https://melpa.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("org" . "https://orgmode.org/elpa/")))
-;; Initialise packages
-(package-initialize)
-
-;; Install use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;; use-package is landed as a default package into emacs 29
-;; In this case we don't need to add a load path
-(when (< (string-to-number emacs-version) 29)
-  (add-to-list 'load-path
-               (package-desc-dir
-                (car (cdr (assq 'use-package package-alist))))))
-(require 'use-package)
-(setq use-package-always-ensure t)
 
 ;; Defines a global key map which always overrides other keybindings
 ;; https://stackoverflow.com/q/34554427
@@ -150,16 +151,17 @@
   t nil my:global-key-map)
 (my:global-key-mode t)
 
-(defadvice load (after my:keybindings-priority)
+(defun my:load-with-keybindings-priority
+    (file &optional noerror nomessage nosuffix must-suffix)
   "Try to ensure that my keybindings always have priority."
   (if (not (eq (car (car minor-mode-map-alist)) 'my:global-key-mode))
       (let* ((mykeys (assq 'my:global-key-mode minor-mode-map-alist))
              (mod (assq-delete-all 'my:global-key-mode minor-mode-map-alist)))
         (add-to-list 'minor-mode-map-alist mykeys))))
-(ad-activate 'load)
+(advice-add 'load :after #'my:load-with-keybindings-priority)
 
 ;; install & configure packages
-(let* ((dir (concat user-emacs-directory "config"))
+(let* ((dir (locate-user-emacs-file "config"))
        (files
         (when (file-directory-p dir)
           (remq nil
