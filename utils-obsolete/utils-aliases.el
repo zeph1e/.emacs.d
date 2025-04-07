7641223e76aca04afad6ac03913a74db8211a992
Yunsik Jang <z3ph1e@gmail.com>
2025-04-07 14:26:24 +0900

    Remove old config files having nothing to migrate

--- utils-obsolete/utils-aliases.el
+++
@@ -1,9 +0,0 @@
-;;; utils-aliases.el
-
-;; Written by Yunsik Jang <doomsday@kldp.org>
-;; You can use/modify/redistribute this freely
-
-(defalias 'ke! #'save-buffers-kill-emacs
-  "Save buffers and kill emacs.")
-
-(provide 'utils-aliases)
