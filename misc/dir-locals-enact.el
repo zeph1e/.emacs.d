7a38ca5fd4986e9bbc6a1981c9d88496d08480a3
Yunsik Jang <z3ph1e@gmail.com>
2025-04-30 13:40:28 +0900

    Set projectile project root where .dir-locals.el is

--- misc/dir-locals-enact.el
+++ misc/dir-locals-enact.el
@@ -2,6 +2,13 @@
          (tab-width . 4)
          (indent-tabs-mode . t)
          (js-indent-level . 4)
-         (css-indent-offset . 4)))
+         (css-indent-offset . 4)
+         (eval . (setq-local projectile-project-root
+                             (let ((root (dir-locals-find-file
+                                          dir-locals-file)))
+                               (cond
+                                ((listp root) (car root))
+                                ((stringp root) root)
+                                (t nil)))))))
  (lisp-data-mode . ((indent-tabs-mode . nil)))
  (auto-mode-alist . (("\\.js\\'" . js-jsx-mode))))
