c2a6aa7c1a0a244796650ea9dc8736c1b4253ec6
Yunsik Jang <z3ph1e@gmail.com>
2026-04-07 20:40:53 +0900

    Add claude config

---
+++ config/claude.el
@@ -0,0 +1,9 @@
+(use-package eat)
+
+(use-package claude-code-ide
+  :straight (:type git :host github :repo "manzaltu/claude-code-ide.el")
+  :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
+  :config
+  (claude-code-ide-emacs-tools-setup) ; Optionally enable Emacs MCP tools
+  :custom
+  (claude-code-ide-terminal-backend 'eat))
