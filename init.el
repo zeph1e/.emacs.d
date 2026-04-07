e4442e7a3b6bb07047900502ac13204fead589a6
Yunsik Jang <z3ph1e@gmail.com>
2026-04-07 20:38:38 +0900

    Add straight

--- init.el
+++ init.el
@@ -88,7 +88,25 @@
       kept-old-versions 2)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; package & use-package initialization
+;; straight & package & use-package initialization
+(defvar bootstrap-version)
+(setq warning-suppress-types '((straight package)))
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+
 (require 'package)
 ;; Add various package archives
 (setq package-archives
