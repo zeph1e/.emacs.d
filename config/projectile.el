8d85a09e8643d8a3574f7e7146409d48177c7b0a
Yunsik Jang <z3ph1e@gmail.com>
2026-07-06 21:36:19 +0900

    Deal with removal of projectile-global-mode

--- config/projectile.el
+++ config/projectile.el
@@ -4,12 +4,12 @@
 ;; You can use/modify/redistribute this freely.
 
 (use-package projectile
-  :init
-  (projectile-global-mode)
   :custom
   (projectile-enable-caching t)
   (projectile-file-exists-remote-cache-expire (* 7 24 60 60)) ; a week
   (projectile-file-exists-local-cache-expire (* 7 24 60 60)) ; a week
   (projectile-completion-system 'helm)
   (projectile-switch-project-action 'projectile-find-file-dwim)
-  (projectile-project-search-path '(("~/Workspace" . 1))))
+  (projectile-project-search-path '(("~/Workspace" . 1)))
+  :hook
+  (after-init . projectile-mode))
