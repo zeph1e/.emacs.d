112ce02da8e2bbc0e5e1aa11c35def6144fe4718
Yunsik Jang <z3ph1e@gmail.com>
2026-08-03 14:45:47 +0900

    Refine rust dev env. & remove header stuffs

--- config/lsp.el
+++ config/lsp.el
@@ -5,6 +5,25 @@
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
+  :init
+  (defun my:lsp-rust-setup-conditional-project ()
+    "Dynamically configure rust-analyzer based on whether a project root exists."
+    (let ((has-project-file
+           (or (locate-dominating-file default-directory "Cargo.toml")
+               (locate-dominating-file default-directory "rust-project.json"))))
+      (if has-project-file
+          (setq-local lsp-rust-analyzer-linked-projects nil)
+        (setq-local lsp-rust-analyzer-linked-projects
+                    `[(:roots ["."]
+                       :crates [(:root_module ,(buffer-file-name)
+                                 :edition "2021"
+                                 :deps []
+                                 :is_workspace_member t)]
+                       :sysroot_src
+                       ,(concat (string-trim-right
+                                 (shell-command-to-string
+                                  "rustup run stable rustc --print sysroot"))
+                                 "/lib/rustlib/src/rust/library"))]))))
   :hook
   ;; (c-mode . lsp)
   ;; (c++-mode . lsp)
@@ -17,6 +36,8 @@
   ;; add there bin/ directory to $PATH environment variable
   (kotlin-mode . lsp-deferred)
   (python-mode . lsp-deferred)
+  (rust-mode . lsp-deferred)
+  (rust-mode . my:lsp-rust-setup-conditional-project)
   (typescript-mode . lsp-deferred)
   (web-mode . lsp-deferred)
   (lsp-mode . lsp-enable-which-key-integration)
@@ -31,6 +52,7 @@
   :custom
   ((lsp-keymap-prefix "C-c C-l")
    (lsp-completion-provider :capf)
+   (lsp-auto-guess-root nil)
    (lsp-ui-sideline-show-diagnostics nil)))
 
 (use-package lsp-ui)
--- config/rust.el
+++ config/rust.el
@@ -3,4 +3,19 @@
 ;; Written by Yunsik Jang <z3ph1e@gmail.com>
 ;; You can use/modify/redistribute this freely.
 
-(use-package rustic)
+(use-package rust-mode
+  :config
+  (require 'cl-macs)
+  (cl-flet ((which #'(lambda (bin)
+                   (string-trim-right
+                    (shell-command-to-string
+                     (concat "rustup which " bin))))))
+    (setq-default rust-rustfmt-bin (which "rustfmt")
+                  rust-cargo-bin (which "cargo")))
+  :ensure-system-package
+  ((rustup . "sudo apt install -y rustup")
+   (rustc . "rustup default --stable")
+   (rust-analyzer . "rustup component add rust-analyzer rust-src"))
+  :custom
+  (lsp-rust-analyzer-diagnostics-enable-experimental t)
+  :after (lsp))
