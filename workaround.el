ec64d3a62f35e5a9118442b5a58c560e402f1898
Yunsik Jang <z3ph1e@gmail.com>
2026-08-06 18:45:34 +0900

    Workaround annoying complaints about nil in face attribute

--- workaround.el
+++ workaround.el
@@ -2,3 +2,17 @@
 ;; To fix issue in dictionaries-common:
 ;; https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=968955
 (setq ispell-menu-map-needed t)
+
+(defun my:normalize-nil-face-attributes (orig-fun face frame &rest args)
+  "Replace :foreground nil and :background nil with 'unspecified."
+  (let ((plist (copy-sequence args)))
+    (let ((tail plist))
+      (while tail
+        (when (or (eq (car tail) :foreground)
+                  (eq (car tail) :background))
+          (when (null (cadr tail))
+            (setcar (cdr tail) 'unspecified)))
+        (setq tail (cddr tail))))
+    (apply orig-fun face frame plist)))
+
+(advice-add 'set-face-attribute :around #'my:normalize-nil-face-attributes)
