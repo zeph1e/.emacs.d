2ac08d24466bf6441bad476da83d9cfdfb0886b8
Yunsik Jang <z3ph1e@gmail.com>
2026-09-14 11:41:18 +0900

    Fix invalid upesp+ hook handler

--- init.el
+++ init.el
@@ -156,7 +156,7 @@
     (exec-path-from-shell-initialize))
   :hook
   ;; pull updated (if any) variables into current Emacs session
-  (upesp+:command-executed . exec-path-from-shell-initialize))
+  (upesp+:command-executed . (lambda (_) (exec-path-from-shell-initialize))))
 
 ;; add local package path to load path
 (let ((default-directory (locate-user-emacs-file "plugins")))
