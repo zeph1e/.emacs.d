46155c23bd57949f0bdc77e9a76aebf4cb2de78c
Yunsik Jang <z3ph1e@gmail.com>
2026-09-18 09:22:32 +0900

    let builtin package be upgraded

--- init.el
+++ init.el
@@ -136,7 +136,8 @@
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "https://melpa.org/packages/")))
+        ("melpa" . "https://melpa.org/packages/"))
+      package-install-upgrade-built-in t)
 ;; Initialise packages
 (package-initialize)
 
